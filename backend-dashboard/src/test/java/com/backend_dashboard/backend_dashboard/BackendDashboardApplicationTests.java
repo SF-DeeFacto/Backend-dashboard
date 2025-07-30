168e61d18bb99daf3a0d74c688c8129c4b34ea7f
Jeung9711 <134358261+Jeung9711@users.noreply.github.com>
2025-07-30 13:15:35 +0900

    init commit

---
+++ backend-dashboard/src/test/java/com/backend_dashboard/backend_dashboard/BackendDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_dashboard.backend_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
